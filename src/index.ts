9011b37f2cd78eb46b2dda2f0eec951f6f0e583f
Daniel Yang <shu.minyang@hotmail.com>
2023-01-31 12:05:54 -0500

    new post endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,27 @@ import express from "express";
 const app = express();
 
 app.get("/", (_, res) => {
+  console.log("GET /");
   res.sendStatus(200);
 });
 
 app.get("/.well-known/apollo/server-health", (_, res) => {
   res.sendStatus(200);
+});
+
+app.post("/sign-in-example", (_, res) => {
+  const val = Math.random() - 0.05;
+  if (val < 0.45) {
+    res.send(`val: ${val}`).sendStatus(200);
+  } else if (val >= 0.45 && val < 0.6) {
+    res.status(400);
+    res.send(`Throwing 400 error: ${val}`);
+  } else if (val >= 0.6 && val < 0.8) {
+    res.status(401);
+    res.send(`Throwing 401 error: ${val}`);
+  } else {
+    throw new Error(`Throwing 500 error: ${val}`);
+  }
 });
 
 app.get("/400-error", (_, res) => {
